1c931799c5fbca5c4020c06a45a8452c8eaae53b
kuroun <sk_roun@yahoo.com>
2013-12-26 00:09:09 +0700

    Design layout with corusel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlyingFood::Application.config.secret_token = '25f2fef3880988bfa29086f667f6ac6eed2b4a28a4327866886e70014780d88f11c03cb149b9a790b5f31d2c210296551d49cf9ed2d526e0176d817b18d46138'
